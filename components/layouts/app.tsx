3e4cf3489290618b732fd655b505fd5ac3a81bec
bl4ko <go3969@student.uni-lj.si>
2023-05-07 14:55:05 +0200

    Fixing font sizes for applayout

--- components/layouts/app.tsx
+++ components/layouts/app.tsx
@@ -5,7 +5,6 @@
  *   - HTML head with meta tags, description, and title.
  *   - NavBar component for navigation.
  *   - Container component to constrain and style the page content.
- *   - Lazy-loaded Model component.
  *   - Footer component.
  *
 */
@@ -33,7 +32,7 @@ export default function AppLayout({ children, router }: LayoutProps) {
 
             <NavBar path={router.asPath} />
 
-            <div className="max-w-2xl pt-14 mx-auto text-sm sm:text-text-sm md:text-text-base lg:text-lg xl:text-xl lg:px-8 sm:px-6 xs:px-4">
+            <div className="text-base max-w-2xl pt-14 mx-auto xs:px-4 xss:px-3">
 
                 <div className="relative z-0">
                     <EarthCanvas />
