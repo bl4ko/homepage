cba50117adbf5dbeb1b3f045b145c2fc01cfffac
bl4ko <go3969@student.uni-lj.si>
2023-05-22 11:46:51 +0200

    Preparing docker deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-}
+    output: "standalone" // https://stackoverflow.com/a/73915394
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
