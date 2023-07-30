4c940b825544a96d27f99bfd8c321feb2719c8c9
bl4ko <go3969@student.uni-lj.si>
2023-07-30 17:46:27 +0200

    Adding tests

--- tests/test-projects.spec.ts
+++ tests/test-projects.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test("Should navigate to projects page", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await page.click("text=Projects");
+  await page.getByRole("link", { name: "Projects" }).click();
   await expect(page).toHaveURL("http://localhost:3000/projects");
-  await expect(page.locator("h1")).toContainText("Projects");
+  await expect(page.getByRole("heading", { name: "Projects" })).toContainText(
+    "Projects"
+  );
 });
