27e02e906beb009b1c5cbfc4233acbeca2d7c5ca
bl4ko <go3969@student.uni-lj.si>
2023-08-15 20:14:04 +0200

    Fixing playwright webServer to run npm run dev

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: "standalone" // https://stackoverflow.com/a/73915394
+  reactStrictMode: true,
+  output: "standalone", // Requirement for docker: https://stackoverflow.com/a/70704689, reduces image size drastically: https://www.dave-beaumont.co.uk/2022/02/22/reducing-the-dockerized-image-size-of-a-next-js-kontent-integrated-website-with-next-standalone-functionality-in-v12-1/
 };
 
 module.exports = nextConfig;
