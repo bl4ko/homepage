41af6c13daaffdab47db69deb058893b494f4881
bl4ko <go3969@student.uni-lj.si>
2023-08-28 14:45:34 +0200

    Fixing responsive padding for navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,128 +5,152 @@ import ThemeToggleButton from "./icons/theme-toggle-button";
 import { motion } from "framer-motion";
 
 const mobileMenuVariants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: "100%" },
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "100%" },
 };
 
 interface LinkItemProps {
-    href: string;
-    path: string;
-    children: React.ReactNode;
-    target?: string;
-    className?: string;
-    toggleMenu?: () => void;
+  href: string;
+  path: string;
+  children: React.ReactNode;
+  target?: string;
+  className?: string;
+  toggleMenu?: () => void;
 }
 
-function LinkItem({ href, path, children, className, toggleMenu, ...props }: LinkItemProps) {
-    const active = path === href;
+function LinkItem({
+  href,
+  path,
+  children,
+  className,
+  toggleMenu,
+  ...props
+}: LinkItemProps) {
+  const active = path === href;
 
-    function handleClick() {
-        if (toggleMenu) {
-            toggleMenu();
-        }
+  function handleClick() {
+    if (toggleMenu) {
+      toggleMenu();
     }
+  }
 
-    return (
-        <Link className={`${active ? 'bg-aqua' : ''} px-2 py-1 text-textPrimary transition-colors duration-200 ${className}`}
-            href={href}
-            {...props}
-            onClick={handleClick}
-        >
-            {children}
-        </Link >
-    );
-};
+  return (
+    <Link
+      className={`${
+        active ? "bg-aqua" : ""
+      } px-2 py-1 text-textPrimary transition-colors duration-200 ${className}`}
+      href={href}
+      {...props}
+      onClick={handleClick}
+    >
+      {children}
+    </Link>
+  );
+}
 
 export default function Navbar(props: any) {
-    const { path } = props;
-    const [isOpen, setIsOpen] = useState(false);
+  const { path } = props;
+  const [isOpen, setIsOpen] = useState(false);
 
-    function toggleMenu() {
-        setIsOpen(!isOpen);
-        console.log(isOpen);
-    }
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  }
 
-    return (
-        <nav className="items-center fixed w-full z-10 backdrop-blur py-3">
-            <div className="w-full flex justify-between mx-auto items-center px-4 max-w-6xl">
-                <div className="hidden sm:flex flex-1 space-x-4 md:mt-0">
-                    <LinkItem href="/" path={path}>
-                        Home
-                    </LinkItem>
-                    <LinkItem href="/projects" path={path}>
-                        Projects
-                    </LinkItem>
-                    <LinkItem href="/experience" path={path}>
-                        Experience
-                    </LinkItem>
-                    <LinkItem href="/technologies" path={path}>
-                        Technologies
-                    </LinkItem>
-                    <LinkItem
-                        target="_blank"
-                        href="https://github.com/bl4ko/homepage"
-                        path={path}
-                        className="inline-flex items-center gap-1 pl-1"
-                    >
-                        <GithubIcon />
-                        Code
-                    </LinkItem>
-                </div>
+  return (
+    <nav className="items-center fixed w-full z-10 backdrop-blur md:py-3 sm:py-2">
+      <div className="w-full flex justify-between mx-auto items-center px-4 max-w-6xl">
+        <div className="hidden sm:flex flex-1 space-x-4 md:mt-0">
+          <LinkItem href="/" path={path}>
+            Home
+          </LinkItem>
+          <LinkItem href="/projects" path={path}>
+            Projects
+          </LinkItem>
+          <LinkItem href="/experience" path={path}>
+            Experience
+          </LinkItem>
+          <LinkItem href="/technologies" path={path}>
+            Technologies
+          </LinkItem>
+          <LinkItem
+            target="_blank"
+            href="https://github.com/bl4ko/homepage"
+            path={path}
+            className="inline-flex items-center gap-1 pl-1"
+          >
+            <GithubIcon />
+            Code
+          </LinkItem>
+        </div>
 
-                <div className="flex flex-wrap justify-end items-center w-full">
-                    <ThemeToggleButton />
-                    <div className="ml-1 sm:hidden">
-                        <button onClick={toggleMenu} className="focus:outline-none" aria-label="Options">
-                            <span className="material-symbols-outlined text-3xl">
-                                menu
-                            </span>
-                        </button>
-                        <motion.div
-                            initial="closed"
-                            animate={isOpen ? "open" : "closed"}
-                            variants={mobileMenuVariants}
-                            transition={{ duration: 0.5 }}
-                            className="absolute right-0 mt-2 w-48 bg-tertiary rounded-md shadow-lg mr-1 border-2 text-sm"
-                        >
-                            <ul>
-                                <li className="px-4 py-2">
-                                    <LinkItem href="/" path={path} toggleMenu={toggleMenu}>
-                                        Home
-                                    </LinkItem>
-                                </li>
-                                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
-                                    <LinkItem href="/projects" path={path} toggleMenu={toggleMenu}>
-                                        Projects
-                                    </LinkItem>
-                                </li>
-                                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
-                                    <LinkItem href="/experience" path={path} toggleMenu={toggleMenu}>
-                                        Experience
-                                    </LinkItem>
-                                </li>
-                                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
-                                    <LinkItem href="/technologies" path={path} toggleMenu={toggleMenu}>
-                                        Technologies
-                                    </LinkItem>
-                                </li>
-                                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
-                                    <LinkItem
-                                        target="_blank"
-                                        href="https://github.com/bl4ko/homepage"
-                                        path={path}
-                                        className="inline-flex items-center gap-1 pl-1"
-                                        toggleMenu={toggleMenu}
-                                    >
-                                        <GithubIcon />
-                                        Code
-                                    </LinkItem>
-                                </li>
-                            </ul>
-                        </motion.div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="flex flex-wrap justify-end items-center w-full">
+          <ThemeToggleButton />
+          <div className="ml-1 sm:hidden">
+            <button
+              onClick={toggleMenu}
+              className="focus:outline-none"
+              aria-label="Options"
+            >
+              <span className="material-symbols-outlined text-3xl">menu</span>
+            </button>
+            <motion.div
+              initial="closed"
+              animate={isOpen ? "open" : "closed"}
+              variants={mobileMenuVariants}
+              transition={{ duration: 0.5 }}
+              className="absolute right-0 mt-2 w-48 bg-tertiary rounded-md shadow-lg mr-1 border-2 text-sm"
+            >
+              <ul>
+                <li className="px-4 py-2">
+                  <LinkItem href="/" path={path} toggleMenu={toggleMenu}>
+                    Home
+                  </LinkItem>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
+                  <LinkItem
+                    href="/projects"
+                    path={path}
+                    toggleMenu={toggleMenu}
+                  >
+                    Projects
+                  </LinkItem>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
+                  <LinkItem
+                    href="/experience"
+                    path={path}
+                    toggleMenu={toggleMenu}
+                  >
+                    Experience
+                  </LinkItem>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
+                  <LinkItem
+                    href="/technologies"
+                    path={path}
+                    toggleMenu={toggleMenu}
+                  >
+                    Technologies
+                  </LinkItem>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700">
+                  <LinkItem
+                    target="_blank"
+                    href="https://github.com/bl4ko/homepage"
+                    path={path}
+                    className="inline-flex items-center gap-1 pl-1"
+                    toggleMenu={toggleMenu}
+                  >
+                    <GithubIcon />
+                    Code
+                  </LinkItem>
+                </li>
+              </ul>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
