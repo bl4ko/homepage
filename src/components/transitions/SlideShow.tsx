f80447b86d742863bfb76b719865213069d3d99f
bl4ko <gasperoblak@bl4ko.com>
2023-10-23 20:24:38 +0200

    Adding hacky workaround for page transitions

--- src/components/transitions/SlideShow.tsx
+++ src/components/transitions/SlideShow.tsx
@@ -1,10 +1,23 @@
 "use client";
-
+import { useRef, useContext } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
-// Animations bug with nextjs for component exit: https://github.com/vercel/next.js/issues/49279
-// Possible workaround: https://github.com/vercel/next.js/issues/49596#issuecomment-1752141334
+// Unresolved issued, framer motion on exit don't work: https://github.com/vercel/next.js/issues/49279
+// Workaround with frozen router: https://github.com/vercel/next.js/issues/49279#issuecomment-1718177583
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+function FrozenRouter(props: { children: React.ReactNode }) {
+  const context = useContext(LayoutRouterContext);
+  const frozen = useRef(context).current;
+
+  return (
+    <LayoutRouterContext.Provider value={frozen}>
+      {props.children}
+    </LayoutRouterContext.Provider>
+  );
+}
+
 export default function SlideShow({ children }: { children: React.ReactNode }) {
   const path = usePathname();
   return (
@@ -23,8 +36,38 @@ export default function SlideShow({ children }: { children: React.ReactNode }) {
         exit={{ opacity: 0, x: 0, y: 45 }}
         transition={{ duration: 1, type: "easeInOut" }}
       >
-        {children}
+        <FrozenRouter>{children}</FrozenRouter>
       </motion.article>
     </AnimatePresence>
   );
 }
+
+// // Uncomment this when issues is resolved:
+// "use client";
+
+// import { AnimatePresence, motion } from "framer-motion";
+// import { usePathname } from "next/navigation";
+
+// export default function SlideShow({ children }: { children: React.ReactNode }) {
+//   const path = usePathname();
+//   return (
+//     <AnimatePresence
+//       mode="wait"
+//       onExitComplete={() => {
+//         if (typeof window !== "undefined") {
+//           window.scrollTo({ top: 0 });
+//         }
+//       }}
+//     >
+//       <motion.article
+//         key={path}
+//         initial={{ opacity: 0, x: 0, y: 45 }}
+//         animate={{ opacity: 1, x: 0, y: 0 }}
+//         exit={{ opacity: 0, x: 0, y: 45 }}
+//         transition={{ duration: 1, type: "easeInOut" }}
+//       >
+//         {children}
+//       </motion.article>
+//     </AnimatePresence>
+//   );
+// }
