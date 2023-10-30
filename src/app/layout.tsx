7cb3e4146d3bebf255ff613276aca03b0978159e
bl4ko <gasperoblak@bl4ko.com>
2023-10-30 16:13:57 +0100

    Adding meta fields, using new Metadata API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import SlideShow from "@/src/components/transitions/SlideShow";
 
 import Footer from "@/src/components/footer";
 import Navbar from "@/src/components/navbar";
+import Cursor from "@/src/components/cursor/Cursor";
 import "./globals.css";
 
 import { Metadata } from "next";
@@ -12,6 +13,10 @@ import StarsCanvas from "../components/canvas/Stars";
 export const metadata: Metadata = {
   title: "Bl4ko",
   description: "Bl4ko's homepage",
+  authors: [{ name: "bl4ko" }],
+  keywords: "Bl4ko, Bl4ko's homepage",
+  robots: "index, follow",
+  icons: "/images/profile_no_background.png",
 };
 
 export default function RootLayout({
@@ -23,24 +28,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <Head>
-        {" "}
-        <link rel="icon" href="/images/profile_no_background.png" />
-        <meta charSet="UTF-8" />
-        <meta name="description" content="Bl4ko's homepage" />
-        <meta name="author" content="Bl4ko" />
-        <meta name="robots" content="index, follow" />
-        <meta name="keywords" content="Bl4ko, Bl4ko's homepage" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=swap"
-        />
-      </Head> */}
       <body>
         <main className="relative pb-8 h-full">
           <ThemeProvider>
             <StarsCanvas />
             <Navbar />
+            <Cursor />
             <div className="text-base md:text-lg max-w-6xl pt-14 mx-auto xl:px-16 lg:px-12 md:px-10 xs:px-8 px-4">
               <EarthCanvas />
               <SlideShow>{children}</SlideShow>
