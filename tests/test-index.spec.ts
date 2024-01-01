2848451ac84f05f7bb8814c5b954d7953fe16db0
bl4ko <gasperoblak@bl4ko.com>
2024-01-01 16:30:41 +0100

    Fixing hardcoded year in test

--- tests/test-index.spec.ts
+++ tests/test-index.spec.ts
@@ -5,7 +5,11 @@ test("test", async ({ page }) => {
   await page.getByLabel("Toggle theme").click();
   await expect(page.locator("html")).toHaveAttribute("data-theme", "light");
   await page.getByRole("img", { name: "Bl4ko" }).click();
-  await page.getByText("© 2023 Bl4ko. Copy as much as you want.").click();
+  await page
+    .getByText(
+      `© ${new Date().getFullYear()} Bl4ko. Copy as much as you want.`,
+    )
+    .click();
   await page.getByRole("navigation").click();
   await page.getByText("Hello, I'm a tech enthusiast from Slovenia").click();
 });
