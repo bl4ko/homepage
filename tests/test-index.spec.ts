31bf39c740fc3aca074ec26670a98ccef45ff029
bl4ko <gasperoblak@bl4ko.com>
2024-01-14 15:30:27 +0100

    Removing playwright upload

--- tests/test-index.spec.ts
+++ tests/test-index.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("Test index page", async ({ page }) => {
   await page.goto("/");
   await page.getByLabel("Toggle theme").click();
   await expect(page.locator("html")).toHaveAttribute("data-theme", "light");
